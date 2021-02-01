831e3c4827e7a580b1f1d5d68bbcd71108f90431
Juan Pablo <esperix@gmail.com>
2021-02-01 06:40:41 +0100

    List of movies
    Got a list of top 20 movies

---
+++ src/app/movie-list/movie-list.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {TmdbService} from '../sevices/tmdb.service';
+
+
+@Component({
+  selector: 'app-movie-list',
+  templateUrl: './movie-list.component.html',
+  styleUrls: ['./movie-list.component.scss']
+})
+export class MovieListComponent implements OnInit {
+  movies: any[] = [];
+  pictures: any[] = [];
+
+  constructor(private tmdbService: TmdbService) {
+  }
+
+  ngOnInit(): void {
+    this.tmdbService.getPopularMovies()
+      .subscribe((response: any) => {
+          console.log(response);
+          this.movies = response.results;
+          console.log(this.movies);
+        });
+  }
+}
