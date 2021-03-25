61d05a98b688d835b928b20ed3cb787f48fb1679
Juan Pablo Orellana <49680187+jpablo1980@users.noreply.github.com>
2021-03-25 22:47:48 +0100

    Deleted component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchMovieComponent  } from './search-movie/search-movie.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
-import { MovieDetailComponent  } from './movie-detail/movie-detail.component';
 
 import { HttpClientModule } from '@angular/common/http';
 
@@ -14,7 +13,6 @@ import { HttpClientModule } from '@angular/common/http';
     AppComponent,
     MovieListComponent,
     SearchMovieComponent,
-    MovieDetailComponent
   ],
   imports: [
     BrowserModule,
